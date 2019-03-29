83a3ff02a774814cc04d21999ebe0f38a7e6223e
Melody_Vsl <mld_azad@yahoo.com>
2019-03-29 23:54:42 +0100

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,38 @@
+//imrc
+import React, { Component } from 'react';
+
+//cc
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div> //<div> be jaye div ino mishe nevesht ta do ta div nadashte bashim to console
+    );
+  }
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    classes += this.props.counter.value === 0 ? 'warning' : 'primary';
+    return classes;
+  }
+
+  //this refrence to the current object...between {} for span we can use any valid java script expression
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? 'Zero' : value;
+  }
+}
+export default Counter;
